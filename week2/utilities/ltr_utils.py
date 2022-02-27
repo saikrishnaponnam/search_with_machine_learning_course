339d167940e90e911f64abed6c0457fcaf919856
saikrishnaponnam <saikrishnaponnam77@gmail.com>
2022-02-27 18:13:42 +0000

    level 2

--- week2/utilities/ltr_utils.py
+++ week2/utilities/ltr_utils.py
@@ -15,7 +15,7 @@ def create_rescore_ltr_query(user_query, query_obj, click_prior_query, ltr_model
                     "sltr": {
                         "params": {
                             "keywords": user_query,
-                            # "click_prior_query": click_prior_query
+                            "click_prior_query": click_prior_query
                         },
                         "model": ltr_model_name,
                         "store": ltr_store_name
